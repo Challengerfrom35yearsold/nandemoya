066befcc54eaa4deca6bcbd275da3eea6acd07d1
Challengerfrom35yearsold <124878663+Challengerfrom35yearsold@users.noreply.github.com>
2023-08-12 02:47:00 +0000

    [Update]新規登録画面・ログイン画面のレイアウト調整(customer,shop,admin),[Update]マイページ・登録情報のレイアウト変更(customer,shop,admin)

--- app/controllers/shop/registrations_controller.rb
+++ app/controllers/shop/registrations_controller.rb
@@ -3,6 +3,10 @@
 class Shop::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    shop_shop_page_new_path
+  end
+
   def after_sign_in_path_for(resource)
     shop_path
   end
