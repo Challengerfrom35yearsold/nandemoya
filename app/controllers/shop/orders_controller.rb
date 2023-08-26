8f41315cf8751348ffa6bd1a47a834e4e81488d0
Challengerfrom35yearsold <124878663+Challengerfrom35yearsold@users.noreply.github.com>
2023-08-26 14:45:14 +0000

    [Update]他ユーザーからの制限を削除(shop/orders/show)

--- app/controllers/shop/orders_controller.rb
+++ app/controllers/shop/orders_controller.rb
@@ -13,10 +13,5 @@ class Shop::OrdersController < ApplicationController
       @total_price = @total_price + price * amount
     end
     @order_details = @order.order_details.includes(:item).where(items: {shop_id: current_shop.id}).page(params[:page])
-    @order_details_first = @order_details.first
-    @order_details_first_shop_id = @order_details_first.item.shop.id
-    unless @order_details_first_shop_id == current_shop.id
-      redirect_to shop_orders_path
-    end
   end
 end
