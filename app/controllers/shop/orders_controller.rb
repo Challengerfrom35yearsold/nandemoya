1ddbcfb3485642b160b7751867bb3b1246f328b6
Challengerfrom35yearsold <124878663+Challengerfrom35yearsold@users.noreply.github.com>
2023-09-27 06:31:20 +0000

    [Update]shopのorders_controllerのshowアクションに他ユーザーの排他制御を追加

--- app/controllers/shop/orders_controller.rb
+++ app/controllers/shop/orders_controller.rb
@@ -6,6 +6,9 @@ class Shop::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details.includes(:item).where(items: {shop_id: current_shop.id})
+    if @order_details.blank?
+      redirect_to shop_orders_path
+    end
     @total_price = 0
     @order_details.each do |order_detail|
       price = (order_detail.price*1.1).floor
